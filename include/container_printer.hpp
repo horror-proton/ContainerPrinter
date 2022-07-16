e7d5376ed9a7d8cdbae982a25f41b22bd3c90c58
Horror Proton <107091537+horror-proton@users.noreply.github.com>
2022-07-16 22:37:31 +0800

    Use tuple printer, format code

--- include/container_printer.hpp
+++ include/container_printer.hpp
@@ -2,50 +2,66 @@
 #include <tuple>
 #include <iterator>
 
-
 #define CONTAINER_PRINTER_STREAM_INSERTION cp_stream_insertion
 
+namespace container_printer {
+
 template<typename Stream, typename T, typename Enable = void>
-struct has_stream_insertion : std::false_type {
-};
+struct has_stream_insertion : std::false_type {};
 
 template<typename S, typename T>
-struct has_stream_insertion<S, T, std::void_t<decltype(std::declval<S>() << std::declval<T>())>> : std::true_type {
-};
+struct has_stream_insertion<S, T, std::void_t<decltype(std::declval<S>() << std::declval<T>())>> : std::true_type {};
 
 template<typename T, typename Enable = void>
-struct is_range_expression : std::false_type {
-};
+struct is_range_expression : std::false_type {};
 
 template<typename T>
 struct is_range_expression<T, std::enable_if_t<
-                              std::is_convertible_v<
-                              typename std::iterator_traits<decltype(begin(std::declval<T>()))>::iterator_category,
+    std::is_convertible_v<
+        typename std::iterator_traits<decltype(begin(std::declval<T>()))>::iterator_category,
         std::forward_iterator_tag
->
+    >
 >> : std::true_type {
 };
 
 template<typename T, typename Enable = void>
-struct is_tuple : std::false_type {
-};
+struct is_tuple : std::false_type {};
 
 template<typename T>
-struct is_tuple<T, std::void_t<decltype(get<0>(std::declval<T>())), decltype(std::tuple_size<T>{})>>
-: std::true_type {
-};
+struct is_tuple<T, std::void_t<decltype(get<0>(std::declval<T>())), std::tuple_size<T>>> : std::true_type {};
 
 // TODO: size of other tuple
 
-template<std::size_t ...I, typename TupleT, typename Func, typename...Args>
-constexpr auto tuple_for_each_impl(std::index_sequence<I...>, TupleT &t, Func f, Args ...args) {
-    using std::get;
-    (f(get<I>(t), (args)...), ...);
-}
+template<typename Stream, typename T>
+Stream &CONTAINER_PRINTER_STREAM_INSERTION(Stream &, const T &);
 
-template<typename TupleT, typename Func, typename...Args>
-void tuple_for_each(TupleT &t, Func f, Args...other_args) {
-    tuple_for_each_impl(std::make_index_sequence<std::tuple_size_v<TupleT>>{}, t, f, (other_args)...);
+namespace detail {
+
+template<typename Stream, typename TupleT, std::size_t N = std::tuple_size_v<TupleT>, std::size_t I = 0>
+struct tuple_printer_impl {
+    static constexpr void apply(Stream &s, const TupleT &t) {
+        CONTAINER_PRINTER_STREAM_INSERTION(s, ",");
+        CONTAINER_PRINTER_STREAM_INSERTION(s, get<I>(t));
+        tuple_printer_impl<Stream, TupleT, N, I + 1>::apply(s, t);
+    }
+};
+
+template<typename Stream, typename TupleT, std::size_t N>
+struct tuple_printer_impl<Stream, TupleT, N, 0> {
+    static constexpr void apply(Stream &s, const TupleT &t) {
+        CONTAINER_PRINTER_STREAM_INSERTION(s, "[");
+        CONTAINER_PRINTER_STREAM_INSERTION(s, get<0>(t));
+        tuple_printer_impl<Stream, TupleT, N, 1>::apply(s, t);
+    }
+};
+
+template<typename Stream, typename TupleT, std::size_t N>
+struct tuple_printer_impl<Stream, TupleT, N, N> {
+    static constexpr void apply(Stream &s, const TupleT &) {
+        CONTAINER_PRINTER_STREAM_INSERTION(s, "]");
+    }
+};
+
 }
 
 template<typename Stream, typename T>
@@ -56,20 +72,15 @@ Stream &CONTAINER_PRINTER_STREAM_INSERTION(Stream &stream, const T &value) {
     } else if constexpr (is_range_expression<T>::value) {
         CONTAINER_PRINTER_STREAM_INSERTION(stream, "{");
         std::string_view comma{};
-        for (const auto &e: value) {
+        for (const auto &e : value) {
             CONTAINER_PRINTER_STREAM_INSERTION(stream, comma);
             CONTAINER_PRINTER_STREAM_INSERTION(stream, e);
             comma = ", ";
         }
+        CONTAINER_PRINTER_STREAM_INSERTION(stream, "}");
         return stream;
     } else if constexpr (is_tuple<T>::value) {
-        CONTAINER_PRINTER_STREAM_INSERTION(stream, "[ ");
-        std::string_view comma{};
-        tuple_for_each(value, [](const auto &e, Stream &s) {
-            CONTAINER_PRINTER_STREAM_INSERTION(s, e);
-            CONTAINER_PRINTER_STREAM_INSERTION(s, " ");
-        }, std::ref(stream));
-        CONTAINER_PRINTER_STREAM_INSERTION(stream, "]");
+        detail::tuple_printer_impl<Stream, T>::apply(stream, value);
         return stream;
     } else {
         stream << "object";
@@ -77,4 +88,6 @@ Stream &CONTAINER_PRINTER_STREAM_INSERTION(Stream &stream, const T &value) {
     }
     // TODO: constructable to string
     // TODO: has .to_string()
+}
+
 }
